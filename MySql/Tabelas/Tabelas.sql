a81f708894e64ab9cca01230478d87291066f49e
Thiago Fonseca <57327613+ThiagoDevv@users.noreply.github.com>
2020-08-24 14:42:38 -0300

    Update Tabelas.sql

--- MySql/Tabelas/Tabelas.sql
+++ MySql/Tabelas/Tabelas.sql
@@ -4,27 +4,27 @@ use cursos;
 
 create table Pessoa(
 	IdPessoa int primary key auto_increment,
-    nome varchar(30) not null,
-    cpf varchar(14) unique not null
+        nome varchar(30) not null,
+        cpf varchar(14) unique not null
 );
 
 /* creiei uma tabela só para telefones pois uma pessoa pode ter mais de 1 */
 
 create table Telefones(
 	IdTelefones int primary key auto_increment,
-    tipo char(3), 			/* podendo ser celular(cel); comercial(com); residencial(res)*/
-    numero varchar(15),
-    Id_Pessoa int 			/*chave estrangeira de telefones_Pessoa */
+        tipo char(3), 			/* podendo ser celular(cel); comercial(com); residencial(res)*/
+        numero varchar(15),
+        Id_Pessoa int 			/*chave estrangeira de telefones_Pessoa */
 );
 
 create table Cursos(
 	IdCursos int primary key auto_increment,
-    nome varchar(30) not null
+        nome varchar(30) not null
 );
 
 create table Pessoa_Cursos(
 	IdPessoa int,
-    IdCursos int,
+        IdCursos int,
     
     CONSTRAINT PK_Pessoa_Cursos PRIMARY KEY(IdPessoa, IdCursos)
 );
@@ -42,24 +42,3 @@ references cursos(IdCursos);
 alter table Telefones ADD constraint
  FK_Telefones_Pessoa foreign key(Id_Pessoa)
  references pessoa(IdPessoa);
- 
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
