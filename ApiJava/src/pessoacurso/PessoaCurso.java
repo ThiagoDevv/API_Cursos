d7317312942c79fa213663cf6bd73d82b851a061
Thiago Fonseca <57327613+ThiagoDevv@users.noreply.github.com>
2020-08-23 23:53:17 -0300

    Update PessoaCurso.java

--- ApiJava/src/pessoacurso/PessoaCurso.java
+++ ApiJava/src/pessoacurso/PessoaCurso.java
@@ -1,29 +1,8 @@
 package pessoacurso;
 
-import java.util.Scanner;
-import model.bean.Pessoa;
-import model.dao.PessoaDao;
-
 public class PessoaCurso {
 
     public static void main(String[] args) {
-        String nome;
-        String cpf;
-        
-        
-        Scanner in = new Scanner(System.in);
-        Pessoa p = new Pessoa();
-        PessoaDao dao = new PessoaDao();
-        
-        System.out.println("ID: ");
-        int id = in.nextInt();
-        
-        p.setIdPessoa(id);
-        
-        dao.delete(p);
-        
-        
-        
         
     }
     
