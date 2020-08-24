7ddb4942e2f1c7b77aa85a8edf128a9541a3a090
Thiago Fonseca <57327613+ThiagoDevv@users.noreply.github.com>
2020-08-24 04:53:52 -0300

    Update and rename triggers.sql to backup.sql

---
+++ MySql/Triggers/backup.sql
@@ -0,0 +1,20 @@
+/* backup lógivo de pessoa */
+
+
+DELIMITER $
+Create trigger backup_pessoa
+before delete
+on pessoa
+for each row
+begin 
+
+	insert into bkp_pessoa values(null, old.IdPessoa, old.nome, old.cpf );
+	
+end $
+DELIMITER ;
+
+CREATE table bkp_pessoa(
+	Idbkp_pessoa int Primary key auto_increment,
+	nome varchar(30) not null,
+    cpf varchar(14) unique not null
+);
